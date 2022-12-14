7f5e05a664bd1a5528b416944f2962cb53a51c9d
koval-home <kovalenkokv@gmail.com>
2022-12-14 19:34:53 +0300

    Add some logging. Refactor approach how to generate name for temp files.

--- main.py
+++ main.py
@@ -28,18 +28,19 @@ class VK2YaDiskBackup:
                                     'likes': image['likes']['count'],
                                     'size': image['sizes'][-1]['type'],
                                     'url': image['sizes'][-1]['url'],
-                                    'date': image['date']}
+                                    'date': image['date'],
+                                    'id':image['id']}
             return out
         else:
             return None
 
-    def download_files_from_vk(self, url, to_folder='img'):
-        file_name = str(url).split('?size')[0].split('/')[-1]
-        if not os.path.isdir(os.path.join(os.getcwd(), to_folder)):
-            os.mkdir(os.path.join(os.getcwd(), to_folder))
-        file_name = os.path.join(os.getcwd(), to_folder, file_name)
-        print(file_name)
-        r = requests.get(url, stream=True)
+    def download_files_from_vk(self, image: dict, to_folder='img'):
+        file_name = os.path.join(os.getcwd(), to_folder) #str(url).split('?size')[0].split('/')[-1]
+        if not os.path.isdir(file_name):
+            os.mkdir(file_name)
+        file_name = os.path.join(file_name,str(image['id'])+'.jpg')
+        print('create tmp file: ', file_name)
+        r = requests.get(image['url'], stream=True)
         if r.status_code == 200:
             with open(file_name, 'wb') as file:
                 for chunk in r.iter_content(1024):
@@ -47,46 +48,45 @@ class VK2YaDiskBackup:
 
     def yadisk_upload_file(self, image: dict, base_dir='img'):
         folder_resp = self.session.put(f'{self.base_url}/resources?path={base_dir}')
-        if folder_resp.status_code == 409 or folder_resp.status_code == 201:
+        if folder_resp.status_code == 409 or folder_resp.status_code == 201: # is folder created/exists
             file_url = self.session.get(f'{self.base_url}/resources/upload?path={base_dir}/' \
                                         f'{str(image["likes"])}.jpg&overwrite=false')
             yadisk_file_name = f'{str(image["likes"])}.jpg'
-            if file_url.status_code == 409:
+            if file_url.status_code == 409: # already exists
                 file_url = self.session.get(
                     f'{self.base_url}/resources/upload?path={base_dir}/{str(image["likes"])}_' \
-                    f'{datetime.datetime.fromtimestamp(image["date"]).strftime("%Y%m%d")}.jpg&overwrite=false')
+                    f'{datetime.datetime.fromtimestamp(image["date"]).strftime("%Y%m%d")}_{datetime.datetime.today().timestamp()}.jpg&overwrite=false')
                 yadisk_file_name = f'{str(image["likes"])}_' \
-                                   f'{datetime.datetime.fromtimestamp(image["date"]).strftime("%Y%m%d")}.jpg'
-            file_name = str(image['url']).split('?size')[0].split('/')[-1]
-            file_name = os.path.join(os.getcwd(), 'img', file_name)
-            print(image['url'])
+                    f'{datetime.datetime.fromtimestamp(image["date"]).strftime("%Y%m%d")}_{datetime.datetime.today().timestamp()}.jpg'
+            file_name = os.path.join(os.getcwd(), 'img', str(image['id'])+'.jpg')
+            print('read file: ', file_name)
+            print('save to yadisk: ', file_url.json())
             with open(file_name, 'rb') as file:
                 restp = self.session.post(file_url.json()['href'], files={'file': file})
+                print('Status of putting file to YaDisk:', restp.status_code)
                 if restp.status_code == 201:
                     self.log.append({"file_name": yadisk_file_name,
                                      "size": image['size']})
 
 
 if __name__ == "__main__":
-    if os.getenv('token', default=None) is not None and os.getenv('owner_id', default=None) is not None \
-            and os.getenv('YaDisk_token', default=None) is not None:
-        app = VK2YaDiskBackup(vk_token=os.getenv('token', default=None),
-                              vk_owner_id=os.getenv('owner_id', default=None),
-                              yadisk_token=os.getenv('YaDisk_token', default=None))
-        #Базовая работа
-        vk_photo_list = app.get_vk_profile_photos_list()
-        if vk_photo_list != None:
-            for image in vk_photo_list:
-                app.download_files_from_vk(url=vk_photo_list[image]['url'])
-                app.yadisk_upload_file(vk_photo_list[image])
-                with open('logs.json', 'w') as outfile:
-                    json.dump(app.log, outfile)
-        # Загрузка из Альбома
-        vk_photo_list = app.get_vk_profile_photos_list(album_id='276363897')
-        if vk_photo_list != None:
-            for image in vk_photo_list:
-                app.download_files_from_vk(url=vk_photo_list[image]['url'])
-                app.yadisk_upload_file(vk_photo_list[image], base_dir='vk_albom')
-                with open('logs2.json', 'w') as outfile:
-                    json.dump(app.log, outfile)
+    app = VK2YaDiskBackup(vk_token=os.getenv('token', default=None),
+                          vk_owner_id=os.getenv('owner_id', default=None),
+                          yadisk_token=os.getenv('YaDisk_token', default=None))
+    # Базовая работа
+    vk_photo_list = app.get_vk_profile_photos_list()
+    if vk_photo_list != None:
+        for image in vk_photo_list:
+            app.download_files_from_vk(image=vk_photo_list[image])
+            app.yadisk_upload_file(vk_photo_list[image])
+        with open('logs.json', 'w') as outfile:
+            json.dump(app.log, outfile)
+    # Загрузка из Альбома
+    vk_photo_list = app.get_vk_profile_photos_list(album_id='276363897')
+    if vk_photo_list != None:
+        for image in vk_photo_list:
+            app.download_files_from_vk(image=vk_photo_list[image])
+            app.yadisk_upload_file(vk_photo_list[image])
+        with open('logs2.json', 'w') as outfile:
+            json.dump(app.log, outfile)
 
